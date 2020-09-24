9505bce515950ddca33846f43b13e8da356520be
Fabian-Siebels <fabian.siebels@gmail.com>
2020-09-24 11:10:31 +0200

    update

--- server.js
+++ server.js
@@ -40,34 +40,34 @@ bmestart();
 // const t0 = 273.15;
 // var tk = t + t0;
 
-// var a, b
-// if (t >= 0) {
-//     a = 7.5;
-//     b = 237.3;
-// } else if (t < 0) {
-//     a = 7.6;
-//     b = 240.7;
-// }
+var a, b
+if (t >= 0) {
+    a = 7.5;
+    b = 237.3;
+} else if (t < 0) {
+    a = 7.6;
+    b = 240.7;
+}
 
-// // Sättigungsdampfdruck (hPa)
-// var sdd = 6.1078 * Math.pow(10, (a*t)/(b+t));
+// Sättigungsdampfdruck (hPa)
+var sdd = 6.1078 * Math.pow(10, (a*t)/(b+t));
  
-// // Dampfdruck (hPa)
-// var dd = sdd * (r/100);
+// Dampfdruck (hPa)
+var dd = sdd * (r/100);
                 
-// // Wasserdampfdichte bzw. absolute Feuchte (g/m3)
-// af = Math.pow(10,5) * mw/gk * dd/tk; 
+// Wasserdampfdichte bzw. absolute Feuchte (g/m3)
+af = Math.pow(10,5) * mw/gk * dd/tk; 
     
-// // v
-// v = Math.log10(dd/6.1078);
+// v
+v = Math.log10(dd/6.1078);
 
-// // Taupunkttemperatur (°C)
-// td = (b*v) / (a-v);
+// Taupunkttemperatur (°C)
+td = (b*v) / (a-v);
 
 
-// console.log("Dampfdruck = "+ Math.round(dd*100)/100 + " mbar");
-// console.log("Absulute Feuchte = "+ Math.round(af*100)/100 + " g/m³");
-// console.log("Taupunkt = "+ Math.round(td*100)/100 + " °C");
+console.log("Dampfdruck = "+ Math.round(dd*100)/100 + " mbar");
+console.log("Absulute Feuchte = "+ Math.round(af*100)/100 + " g/m³");
+console.log("Taupunkt = "+ Math.round(td*100)/100 + " °C");
 // console.log("Ergebnis= " + sensorErg);
 
 // var dbtp = Math.round(td*100)/100;
@@ -109,4 +109,3 @@ function bmestart() {
         await sensor.close();
     }).catch(console.log);
 }
-
